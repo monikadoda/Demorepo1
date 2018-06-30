f922070a9b45c80a9e738b76acb005ff468555f2
monika <monikadoda1212@gmail.com>
2018-06-30 19:02:39 +0530

    added search page

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,5 +1,14 @@
 package com.qa.pages;
 
 public class LoginPage {
+	
+	public void login()
+	{
+		System.out.println("loginPage");
+	}
+	public void loginsecond()
+	{
+		System.out.println("login page");
+	}
 
 }
